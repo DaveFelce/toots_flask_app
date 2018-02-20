06a4cef13b4318507a80bfeddff4d6507cf99dba
David Felce <davefelce@gmail.com>
2018-02-20 09:21:37 +0000

    last commit

--- toots.py
+++ toots.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import json
 
 app = Flask(__name__)
 
@@ -6,6 +7,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # TODO: render threads!
+    data = json.loads(read_file_from_disk('data/toots.json'))
+    structured_data = make_structured_data(data)
+    # TODO: send structured_data to renderer
     return render_toot(None)
 
 
@@ -29,3 +33,40 @@ def render_toot(toot):
         author_name=author_name,
         text=text,
     )
+
+def read_file_from_disk(filename):
+    with open(filename, mode='r') as file:
+        lines = file.read()
+
+    return lines
+
+def make_structured_data(data):
+    """
+    Create a structured parent/child list of dicts based on id
+
+    :param data: list of dicts
+    :return: structured list of dicts
+    """
+    structured_data = []
+    for node in data:
+        parent_id = node.get('parent_id')
+        if parent_id is None:
+            structured_data.append(node)
+            node['children'] = get_children(node.get('id'), data)
+
+def get_children(parent_id, data):
+    """
+    Recursive function to create child node list for current node
+
+    :param parent_id:
+    :param data: the full original data structure
+    :return: list of children
+    """
+    child_nodes = [d for d in data if d['parent_id'] == parent_id]
+    if not child_nodes:
+        return []
+    else:
+        for child_node in child_nodes:
+            child_node['children'] = get_children(child_node.get('id'), data)
+
+        return child_nodes
