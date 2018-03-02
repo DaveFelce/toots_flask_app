669ed2bcf7e94317fbc6b0b2d0616a72012b211b
David Felce <davefelce@gmail.com>
2018-03-02 13:02:40 +0000

    tests

--- tests/frontend_tests.py
+++ tests/frontend_tests.py
@@ -19,3 +19,12 @@ class FrontendTest(unittest.TestCase):
         self.assertIn("<h3>Here they are</h3>", driver.page_source)
         self.assertIn("Toots!", driver.title)
 
+    def test_authors(self):
+        driver = self.driver
+        driver.get("http://127.0.0.1:5000/authors")
+        self.assertIn('<span><a href="/author/Alice">Alice</a></span>', driver.page_source)
+        self.assertIn("<span>zoot zoot!</span>", driver.page_source)
+
+        charlie_links = driver.find_elements_by_link_text('Charlie')
+        self.assertEqual(len(charlie_links), 13)
+
--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -5,8 +5,6 @@ import os
 import toots
 from utils import *
 
-import pprint
-
 
 class TootsTest(unittest.TestCase):
 
@@ -17,13 +15,18 @@ class TootsTest(unittest.TestCase):
         data = json.loads(read_file_from_disk(self.test_file))
         structured_data = make_structured_data(data)
 
-        pprint.pprint(structured_data)
         self.assertEqual(len(structured_data), 3)
         self.assertEqual(len(structured_data[0].get('children')), 2)
         self.assertEqual(len(structured_data[0]['children'][0].get('children')), 1)
         self.assertEqual(structured_data[0]['children'][1]['children'][0]['children'][0].get('text'), 'zoot choot!')
         self.assertEqual(structured_data[0]['children'][1]['children'][0]['children'][0].get('parent_id'), 97)
 
+    def test_make_authors(self):
+        authors_data = json.loads(read_file_from_disk(os.path.join(toots.app.root_path, 'data', 'authors.json')))
+        authors = make_authors(authors_data)
+        self.assertEqual(len(authors), 10)
+        self.assertEqual(authors[5], 'Fred')
+        self.assertEqual(authors[9], 'Jim')
 
 
 
